43cf788245ba6ba11f9f4d79744183310564e186
typoEXE <116042724+typoEXE@users.noreply.github.com>
2023-03-11 14:23:18 -0800

    Add files via upload

---
+++ Dog.java
@@ -0,0 +1,18 @@
+/**
+ * Name: Tyler Hipolito
+ * Course: cs3560
+ * Problem #5 Part D
+ */
+
+package cs3560_Assignment_2;
+
+public class Dog {
+	private String breed;
+	private String name;
+	private Paw[] paw = new Paw[3];
+	
+	public Dog() {
+		// No args constructor
+	}
+
+}
---
+++ Paw.java
@@ -0,0 +1,18 @@
+/**
+ * Name: Tyler Hipolito
+ * Course: cs3560
+ * Problem #5 Part D
+ */
+
+package cs3560_Assignment_2;
+
+public class Paw {
+	private int position;
+	private Dog dog;
+	
+	public Paw(int position, Dog dog) {
+		this.position = position;
+		this.dog = dog;
+	}
+
+}
