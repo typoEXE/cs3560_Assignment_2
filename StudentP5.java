3dd346556c3fb71f08457a67395ea2574013e1a2
typoEXE <116042724+typoEXE@users.noreply.github.com>
2023-03-11 14:15:38 -0800

    Add files via upload

---
+++ StudentP5.java
@@ -0,0 +1,17 @@
+/**
+ * Name: Tyler Hipolito
+ * Course: cs3560
+ * Problem #5 Part A
+ */
+
+package cs3560_Assignment_2;
+
+public class StudentP5 {
+	private String name;
+	private Transcript transcript;
+
+	public StudentP5(String name, Transcript transcript) {
+		this.name = name;
+		this.transcript = transcript;
+	}
+}
---
+++ Transcript.java
@@ -0,0 +1,17 @@
+/**
+ * Name: Tyler Hipolito
+ * Course: cs3560
+ * Problem #5 Part A
+ */
+
+package cs3560_Assignment_2;
+
+public class Transcript {
+	private String course;
+	private double grade;
+	
+	public Transcript(String course, double grade) {
+		this.course = course;
+		this.grade = grade;
+	}
+}
