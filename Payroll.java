b0af46fdfa3f33dbb2dc4ba37b64c4e5e461fd6b
typoEXE <116042724+typoEXE@users.noreply.github.com>
2023-03-11 14:33:02 -0800

    Add files via upload

---
+++ Payroll.java
@@ -0,0 +1,20 @@
+/**
+ * Name: Tyler Hipolito
+ * Course: cs3560
+ * Problem #5 Part H
+ */
+
+package cs3560_Assignment_2;
+
+public class Payroll {
+	private Worker worker;
+
+	public Payroll() {
+		// No args constructor
+	}
+	
+	public void processPayments(Worker worker) {
+		System.out.println("Payment processed for worker <worker name>");
+	}
+
+}
---
+++ Worker.java
@@ -0,0 +1,18 @@
+/**
+ * Name: Tyler Hipolito
+ * Course: cs3560
+ * Problem #5 Part H
+ */
+
+package cs3560_Assignment_2;
+
+public class Worker {
+	private String name;
+	private double hourlyRate;
+
+	public Worker(String name, double hourlyRate) {
+		this.name = name;
+		this.hourlyRate = hourlyRate;
+	}
+
+}
