7cbe35fe051c594bd9a2ab279cf340fcbf77f8e2
typoEXE <116042724+typoEXE@users.noreply.github.com>
2023-03-11 14:28:24 -0800

    Add files via upload

---
+++ Magazine.java
@@ -0,0 +1,15 @@
+/**
+ * Name: Tyler Hipolito
+ * Course: cs3560
+ * Problem #5 Part F
+ */
+
+package cs3560_Assignment_2;
+
+public class Magazine implements SaleableItem {
+
+	public Magazine() {
+		// No args constructor
+	}
+
+}
---
+++ SaleableItem.java
@@ -0,0 +1,14 @@
+/**
+ * Name: Tyler Hipolito
+ * Course: cs3560
+ * Problem #5 Part F
+ */
+
+package cs3560_Assignment_2;
+
+public interface SaleableItem {
+
+	public static void sellCopy() {
+		System.out.println("Selling a <Specific item here>");
+	}
+}
