fd1b5f5fa2fed030aed48e43a34e02c10343117b
typoEXE <116042724+typoEXE@users.noreply.github.com>
2023-03-11 14:20:56 -0800

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,18 @@
+/**
+ * Name: Tyler Hipolito
+ * Course: cs3560
+ * Problem #5 Part C
+ */
+
+package cs3560_Assignment_2;
+
+public class Player {
+	private String name;
+	private boolean expert;
+
+	public Player(String name, boolean expert) {
+		this.name = name;
+		this.expert = expert;
+	}
+
+}
---
+++ Team.java
@@ -0,0 +1,18 @@
+/**
+ * Name: Tyler Hipolito
+ * Course: cs3560
+ * Problem #5 Part C
+ */
+
+package cs3560_Assignment_2;
+
+import java.util.List;
+
+public class Team {
+	private int code;
+	private List<Player> listPlayers;
+
+	public Team() {
+		// no args constructor
+	}
+}
