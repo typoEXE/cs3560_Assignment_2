3807898c60ef0ca180eb4604a8e40db2f5dcd648
typoEXE <116042724+typoEXE@users.noreply.github.com>
2023-03-11 14:18:25 -0800

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,18 @@
+/**
+ * Name: Tyler Hipolito
+ * Course: cs3560
+ * Problem #5 Part B
+ */
+
+package cs3560_Assignment_2;
+
+public class Book {
+	private String name;
+	private String author;
+	private Course course;
+
+	public Book() {
+		// no args constructor
+	}
+
+}
---
+++ Course.java
@@ -0,0 +1,20 @@
+/**
+ * Name: Tyler Hipolito
+ * Course: cs3560
+ * Problem #5 Part B
+ */
+
+package cs3560_Assignment_2;
+
+import java.util.List;
+
+public class Course {
+	private int code;
+	private List<Book> listBooks;
+	
+	public Course(int code, List<Book> listBooks) {
+		this.code = code;
+		this.listBooks = listBooks;
+	}
+
+}
